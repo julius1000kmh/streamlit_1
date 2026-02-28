57754da4d3776dfd115cb95990175be7a7d4a9db
julius s <155694357+julius1000kmh@users.noreply.github.com>
2026-03-01 00:40:07 +0100

    fixxed cv

--- pages/CV.py
+++ pages/CV.py
@@ -17,7 +17,7 @@ col1, col2, col3, col4, col5= st.columns([1, 3,  12, 3, 1])
 
 with col1:
     if st.button("🏠", use_container_width=True):
-        st.switch_page("Streamlitapp.py")
+        st.switch_page("streamlitapp.py")
 
 
 with col3:
@@ -78,24 +78,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-import base64
-
-def display_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-
-    pdf_display = f"""
-        <iframe 
-            src="data:application/pdf;base64,{base64_pdf}" 
-            width="100%" 
-            height="800px" 
-            type="application/pdf">
-        </iframe>
-    """
-
-    st.markdown(pdf_display, unsafe_allow_html=True)
-with col3:
-    display_pdf("CV2.pdf")
+st.image("cv.jpg", use_container_width=True)
 
 
 
